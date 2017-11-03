3628fe286a7bcfed970ebf4b4ee4a08cacece303
Jack Robinson <robinson.jack2000@gmail.com>
2017-11-03 15:16:00 +0000

    First real draft. Location Works. Weather doesn't.

--- GET.js
+++ GET.js
@@ -1,34 +1,45 @@
-//AIzaSyCGsuDfXsl3gcvQwnl86GpI3nArhiCF4yU
-//https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCGsuDfXsl3gcvQwnl86GpI3nArhiCF4yU
 //takes the inputted postvode
 var form = document.getElementById("postCodeForm");
 form.onsubmit = function(event){
 	event.preventDefault();
 	console.log(event);
 	var postCodeInput = document.getElementById("postCode");
-	console.log(postCodeInput.value);
+	locationRequest(postCodeInput.value);
 }
 
-function locationRequest(data){
+function locationRequest(postCode){
 	//Makes the request to get the data from Geocoder
 	var xhrLoc = new XMLHttpRequest();
 	//Makes the API request
-	xhrLoc.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + event + "key=AIzaSyCGsuDfXsl3gcvQwnl86GpI3nArhiCF4yU")
+	xhrLoc.open("GET", "https://maps.googleapis.com/maps/api/geocode/json?address=" + postCode + "&key=AIzaSyCGsuDfXsl3gcvQwnl86GpI3nArhiCF4yU")
+	//when this bit loads:
 	xhrLoc.onload = function(){
+		//get that bare JSON formatted
 		var locationData = JSON.parse(xhrLoc.response);
-		console.log(locationData);
+		getLatLng(locationData)
 	}
-	xhrLoc.send
+	xhrLoc.send()
 }
 
-function weatherRequest(){
+function getLatLng(results){
+	console.log(results);
+	var location = results["results"][0]["geometry"]["location"]
+	var lat = location["lat"]
+	var lng = location["lng"]
+	weatherRequest(lat, lng)
+	console.log(lat)
+	console.log(lng)
+}
+
+function weatherRequest(lat, lng){
 	//Makes the request to get the data from Dark Skies
 	var xhrWeather = new XMLHttpRequest();
 	//Makes the API request
-	xhrWeather.open("GET", "https://api.darksky.net/forecast/f46a65f5e5124d53891965f3209cbc20/37.8267,-122.4233")
+	xhrWeather.open("GET", "https://api.darksky.net/forecast/f46a65f5e5124d53891965f3209cbc20/" + lat + lng)
 	xhrWeather.onload = function(){
-		var data = JSON.parse(xhrWeather.response);
-		console.log(locadata);
+		var weatherData = JSON.parse(xhrWeather.response);
+		console.log(weatherData);
 	}
 	xhrWeather.send
 }
+
