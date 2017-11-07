a7dc46d592ee0d96bbdf04b8781d8f2959daaa0c
Jack Robinson <robinson.jack2000@gmail.com>
2017-11-07 20:43:11 +0000

    This is the code with the PHP request started.

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
-
+	$response = file_get_contents('https://api.darksky.net/forecast/f46a65f5e5124d53891965f3209cbc20/" + lat + "," + lng');
+	$response = json_decode($response);
 ?>
+<!-- This is some code from StackOverflow -->
+<?php
+$url = 'https://api.darksky.net/forecast/f46a65f5e5124d53891965f3209cbc20/" + lat + "," + lng';
+$data = array('key1' => 'value1', 'key2' => 'value2');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+
+var_dump($result);
+?>
+
+<!-- Look at all of this dank HTML -->
 
 <!DOCTYPE html>
 <html>
 <head>
+	
 	<title>Weather application</title>
 	<meta charset="utf-8"
 	name="viewport" content="width=device-width, initial-scale=1">
@@ -30,6 +53,9 @@
 		Post Code:
 		<input type="text" name="postCode" id="postCode">
 		<input type="submit" name="submit"/>
+	</form>
+	<form>
+		
 	</form>
 	<script src="GET.js"></script>
 </body>
